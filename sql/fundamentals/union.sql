81102623767f4a0726cb947e3ad05b8cdb3c89f5
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2017-11-08 10:26:34 -0800

    +union

---
+++ sql/fundamentals/union.sql
@@ -0,0 +1,20 @@
+SELECT id, item
+FROM license
+WHERE id <= 1000
+UNION
+SELECT id, item
+FROM license 
+WHERE item <= 100
+ORDER BY item
+LIMIT 30;
+
+-- UNION removes duplicates. To retain:
+SELECT id, item
+FROM license
+WHERE id <= 1000
+UNION ALL
+SELECT id, item
+FROM license
+WHERE item <= 100
+ORDER BY item
+LIMIT 30;
